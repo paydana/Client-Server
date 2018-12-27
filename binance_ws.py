1f2a101f6878196f7a598eab65393ce04052f84c
Mo Sharieff <24829978+mosh47@users.noreply.github.com>
2018-12-27 18:12:36 -0500

    Update binance_ws.py

--- binance_ws.py
+++ binance_ws.py
@@ -2,37 +2,27 @@ import websockets
 import asyncio
 import json
 import requests
-import logging
 import time
 import datetime
 
 
-#logging.basicConfig(level=print)
-
 # Timer functions and variables
-
-curr_time = lambda: datetime.datetime.fromtimestamp(int(time.time())).strftime('%m-%d-%Y %H:%M:%S')
-dt = lambda x, y, n: (y - x) / n
 
 tPing = tPong = tStart = tEnd = time.time()
 
-# Parameters
+curr_time = lambda: datetime.datetime.fromtimestamp(int(time.time())).strftime('%m-%d-%Y %H:%M:%S')
+dt = lambda x, y, n: (y - x) / n
+du = lambda x, y, n: '{0:.3f}'.format((y - x) / n)
 
 ohlc = ['1m','3m','5m','15m','30m','1h','2h','4h','6h','8h','12h','1d','3d','1w','1M']
 depth = [5,10,20]
 ping = 3
 hours = 23
 
-# Binance server url
-
 ws_url = 'wss://stream.binance.com:9443'
 rs_url = 'https://www.binance.com/api/v1/'
 
-# Function which builds your sending url
-
-url_build = lambda y, tag: '{}/ws/{}'.format(ws_url, y.lower()) if tag == 'single' else '{}/stream?streams={}'.format(ws_url, '/'.join(y))
-
-# Functions for all websocket streams
+url_build = lambda y, tag: '{}/ws/{}'.format(ws_url, y[0].lower()) if tag == 'single' else '{}/stream?streams={}'.format(ws_url, '/'.join(y))
 
 sstream = {'aggTrade':lambda symbol: '{}@aggTrade'.format(symbol.lower()),
            'trade':lambda symbol: '{}@trade'.format(symbol.lower()),
@@ -41,8 +31,6 @@ sstream = {'aggTrade':lambda symbol: '{}@aggTrade'.format(symbol.lower()),
            'ticker':lambda symbol, all_t: '{}@ticker'.format(symbol.lower()) if all_t == False else '!ticker@arr',
            'book':lambda symbol, level: '{}@depth'.format(symbol.lower()) if level == 'update' else '{}@depth{}'.format(symbol.lower(), level)}
 
-# Function to import REST data
-
 def get_data(datatype):
     if datatype == 'ticks':
         url = rs_url + 'exchangeInfo'
@@ -50,85 +38,77 @@ def get_data(datatype):
         if r.status_code == 200:
             return [i['symbol'] for i in r.json()['symbols']]
 
-# Function which builds your final websocket url
 
-def build_msg():
+def stream(tag, ticks):
+    if tag == 'klines':
+        return [sstream['klines'](i, ticks[1]) for i in ticks[0]]
+    elif tag == 'book':
+        return [sstream['book'](i, ticks[1]) for i in ticks[0]]
+    elif tag == 'trade':
+        return [sstream['trade'](i) for i in ticks[0]]
+    elif tag == 'ticker':
+        return [sstream['ticker'](None, True)] if ticks[0] == True else [sstream['ticker'](ticks[0], False)] 
+    else:
+        return [sstream['aggTrade'](i) for i in ticks[0]]
+
+
+def build_msg(tagger):
     ticks = get_data('ticks')
-    ticks = ticks
-
-    klines = [sstream['klines'](i, '1m') for i in ticks]
-    book = [sstream['book'](i, 20) for i in ticks]
-    trade = [sstream['trade'](i) for i in ticks]
-
-    msg = book
-    
+    msg = stream(tagger[0],[ticks,tagger[1]])
     return url_build(msg, 'multi')
 
-# Parses data and sends it to appropriate channel (* Link Server Script here)
-# -----------------------------------------------------------------------------------------
-async def parse_data(data):
+
+async def parse_data(data, tagger, dx):
     data = json.loads(data)
-    print(' Time: {} | {}\n'.format(curr_time(), data))
-
-    # Link To Server
+    print(' Timestamp: {} | Tag: {} | Run(mins): {} | {}\n'.format(curr_time(), tagger[0], dx, data))
 
 
-# -----------------------------------------------------------------------------------------
+async def exchange_client(timest, tagger):
 
-# Main websocket client function (recieves exchange data)
+    a = b = time.time()
 
-async def exchange_client(loop, timest):
+    tPing, tPong = timest
 
-    tStart, tEnd, tPing, tPong = timest
+    send_url = build_msg(tagger)
 
-    send_url = build_msg()
-
-    txx = time.time()
-    tyy = txx
-    cy = 0
-    
     async with websockets.connect(send_url) as ws:
         while True:
             msg = await ws.recv()
-            await parse_data(msg)
-
-            # Time check to send response pong and refresh socket if shut down
+            dx = du(a, b, 60)
+            await parse_data(msg, tagger, dx)
 
             if dt(tPing, tPong, 60) >= ping + 0.2:
                 ut = await ws.pong()
+                await ws.send(json.dumps({'E':int(time.time()),'e':'pong'}))
                 print(' Time: {} | Pong Sent: {}'.format(curr_time(), ut))
                 tPing = tPong
 
-            if dt(tStart, tEnd, pow(60,2)*24) >= hours:
-                print(' Time: {} | Time to refresh socket...........'.format(curr_time()))
-                tStart = tEnd
-                break
-
+            b = time.time()
             tPong = time.time()
-            tEnd = time.time()
-  
-# Main loop function
 
-def run_socket():
-    while True:
-        tPing = time.time()
-        tPong = tPing
 
-        tStart = tPing
-        tEnd = tPing
+async def start(tasks):
+    await asyncio.wait(tasks)
 
-        print(' Time: {} | Socket connecting..........'.format(curr_time()))
-
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(exchange_client(loop, (tStart, tEnd, tPing, tPong)))
-
-        print(' Time: {} | Socket is rebooting..............'.format(curr_time()))
-        time.sleep(4)
-        run_socket()
-
-# Runs entire program
 
 if __name__ == '__main__':
-    run_socket()
+    while True:
+        count = [tPing, tPong]
+        loop = asyncio.get_event_loop()
+        tasks = [loop.create_task(exchange_client(count,['klines','1m'])),
+                 loop.create_task(exchange_client(count,['book',5])),
+                 loop.create_task(exchange_client(count,['trade',None])),
+                 loop.create_task(exchange_client(count,['ticker',[True, True]])),
+                 loop.create_task(exchange_client(count,['aggTrade',None]))]             
+                                  
+        loop.run_until_complete(start(tasks))
 
+        if dt(tStart, tEnd, pow(60,2)*24) >= hours:
+            print(' Time: {} | Time to refresh socket...........'.format(curr_time()))
+            time.sleep(5)
+            tStart = tEnd = tPing = tPong = time.time()
+            tagger = [tPing, tPong]
+            
+        tEnd = time.time()
+  
 
